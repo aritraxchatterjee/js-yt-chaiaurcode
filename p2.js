cc428082d5f2a6b42645b58c0980a9a3e3c630a8
Aritra Chatterjee <136882850+aritraxchatterjee@users.noreply.github.com>
2023-11-26 03:48:38 +0000

    second commit

---
+++ p2.js
@@ -0,0 +1,12 @@
+//alert(3+3); as we are using node js, not browser
+let name="aritra"
+let age =18
+let isLoggedIn=false
+
+//data types strong, boolean , bigint , null(standalone vale)
+//undefined
+//symbol(mainly used in react)
+//object
+
+console.log(typeof 123);
+console.log(typeof null);//interview question
